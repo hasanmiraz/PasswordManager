a9ebc463db5d9f50b963da526fbc012cec6ad718
hasibulmiraz <hasibul.miraz@northsouth.edu>
2022-10-12 23:01:44 +0600

    encryption bug fix

--- source/main.py
+++ source/main.py
@@ -3,7 +3,15 @@ from cryptography.fernet import Fernet
 from time import sleep
 # password = input("enter password")
 # code = int(input("enter code"))
-pm = PasswordManager("abcd", 12)
+pm = PasswordManager("ABCD", 12)
+print(pm.key)
+# key = Fernet.generate_key()
+# print(key)
+art = "abcdefgh"
+f = Fernet(pm.key)
+token = f.encrypt(art.encode())
+print(token)
+print(f.decrypt(token).decode())
 # pm.get_key()
 # pm.create_file()
 # print(pm.get_key())
--- source/manager.py
+++ source/manager.py
@@ -1,24 +1,30 @@
 from cryptography.fernet import Fernet
 import os
+import pickle
 
 
 class PasswordManager:
-    def __init__(self, password, place):
+    def __init__(self, password, secret_key):
         self.password = password
-        self.place = place
-        # self.key = self.__get_key
-        self.__set_key()
-        self.create_file()
+        self.secret_key = secret_key
+        self.key
+        self.__create_file()
+
+    def encrypt_password(self):
+        pass
+
+    def decrypt_password(self):
+        pass
 
     def __set_key(self):
         password_list = list(self.password)
-        place_number = self.place
-        # print(f"the type is : {type(self.key())}")
-        key_list = list(self.__get_key.lower())
-        places = []
+        place_number = self.secret_key
+        key_list = list(self.__get_key().lower())
+        places = list()
 
+        # the indexes where the password should be placed
         for i in range(len(password_list)):
-            places.append(place_number*(i+1))
+            places.append(place_number * (i + 1))
 
         for index_number in places:
             holder = index_number
@@ -35,12 +41,16 @@ class PasswordManager:
             key_list[index_number] = password_list[iteration]
             iteration += 1
 
+        return ("".join(key_list)+"=").encode()
         print(key_list)
         print()
-        # pass
+
+    # determining which key should we get
+    def __get_specific_key(self):
+        return int(self.secret_key % 20)
 
     @staticmethod
-    def create_file():
+    def __create_file():
         path1 = "code.txt"
         path2 = "data.txt"
         if not os.path.exists(path1):
@@ -48,15 +58,17 @@ class PasswordManager:
         if not os.path.exists(path2):
             open("data.txt", "x")
 
-    @property
+    # getting that key based on the index got from __get_specific_key
     def __get_key(self):
         key = open("code.txt", "r")
-        return key.read()
+        all_key = key.read()
+        all_key_list = all_key.split("\n")
+        return all_key_list[self.__get_specific_key()]
 
-    def __generate_key(self):
+    @staticmethod
+    def __generate_key():
         file = open("code.txt", "w")
         for i in range(20):
             key = str(Fernet.generate_key())
             key = key[2: -2]
-            file.write(key+"\n")
-
+            file.write(key + "\n")
